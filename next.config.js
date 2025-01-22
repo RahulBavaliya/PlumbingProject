2513ecb78542290accf311097202b60fcbe7ef43
RahulBavaliya <rsbavaliya330@gmail.com>
2025-01-22 14:56:02 +0530

    update

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  basePath: '/PlumbingProject',
-  assetPrefix: '/PlumbingProject',
 };
 
 module.exports = nextConfig;
