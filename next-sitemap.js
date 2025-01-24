194145be3e3ed4520144060b6d822bde3fc7b408
RahulBavaliya <rsbavaliya330@gmail.com>
2025-01-24 18:14:55 +0530

    added sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://plumbing-project.vercel.app/', // Make sure to replace with your actual URL
+    generateRobotsTxt: true, // Optional: Generate a robots.txt file
+    outDir: './out',  // Ensure it generates in the 'out' directory after export
+    sitemapSize: 7000, // Optional: control the number of URLs per sitemap file
+  };
