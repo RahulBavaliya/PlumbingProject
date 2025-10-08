de21e565de20696cd998cdf04e5ce0d6304d222d
Rahul Bavaliya <41228277+RahulBavaliya@users.noreply.github.com>
2025-10-08 10:34:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  basePath: process.env.VERCEL ? '/' : '/PlumbingProject',
+  basePath: process.env.VERCEL ? '/' : '/PlumbingProject/',
   assetPrefix: process.env.VERCEL ? '/' : '/PlumbingProject/',
   trailingSlash: true,
 };
